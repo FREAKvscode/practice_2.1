652aeca3936b61d1953bcfa0faccb60f647ee3d9
Эльдар <eldar228x@mail.ru>
2026-03-24 14:47:52 +0700

    Initial commit

--- task_2.py
+++ task_2.py
@@ -1,28 +1,40 @@
 def students_and_evaluations(students, result):
-    with open(students, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
+    try:
+        with open(students, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
 
-    with open(result, 'w', encoding='utf-8') as f:
-        dict_students = {}
-        for line in lines:
-            student, evaluations = line.strip().split(':')
-            lst_evaluations = [int(evaluation.strip('!@#№;$%:^?&*<?>,./| ')) for evaluation in evaluations.split(',')]
-            dict_students[student] = lst_evaluations
+    except FileNotFoundError:
+        print(f"Файл {students} не найден")
+        return
 
-        average_value = []
-        for student, evaluations in dict_students.items():
-            average = sum(evaluations) / len(evaluations)
-            average_value.append((student, average))
-            if average > 4:
-                f.write(f"{student} - {average}\n")
+    except Exception as e:
+        print(f"Ошибка при чтении файла {students}: {e}")
+        return
 
-    if average_value:
-        max_average = max(average_value, key=lambda x: x[1])[1]
-        top_students = [student for student, average in average_value if average == max_average]
-        print(f"Студенты с наивысшим средним баллом ({max_average}): {', '.join(top_students)}")
-    else:
-        print("Нет студентов с средним баллом выше 4.0")
+    try:
+        with open(result, 'w', encoding='utf-8') as f:
+            dict_students = {}
+            for line in lines:
+                student, evaluations = line.strip().split(':')
+                lst_evaluations = [int(evaluation.strip('@#№$%&!?;:*^<>,.|/ ')) for evaluation in evaluations.split(',')]
+                dict_students[student] = lst_evaluations
 
+            average_value = []
+            for student, evaluations in dict_students.items():
+                average = sum(evaluations) / len(evaluations)
+                average_value.append((student, average))
+                if average > 4:
+                    f.write(f"{student} - {average}\n")
+
+        if average_value:
+            max_average = max(average_value, key=lambda x: x[1])[1]
+            top_students = [student for student, average in average_value if average == max_average]
+            print(f"Студенты с наивысшим средним баллом ({max_average}): {', '.join(top_students)}")
+        else:
+            print("Нет студентов с средним баллом выше 4.0")
+
+    except Exception as e:
+        print(f"Произошла ошибка при обработке данных: {e}")
 
 if __name__ == '__main__':
     students_and_evaluations(students='students.txt', result='result.txt')
